048e5a43d6b894bb7fbe73a5246f524216163a96
unknown <nourhelal78@gmail.com>
2022-07-15 18:35:27 +0200

    updating index.js to modify main and update mongoose to remove deprecated paramters

--- src/index.js
+++ src/index.js
@@ -29,14 +29,18 @@ app.listen(port, () => {
 const Task = require('./models/task')
 const User = require('./models/user')
 
-const main = async () => {
-    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
+/// const main = async () => {
+//     // const task = await Task.findById('5c2e505a3253e18a43e612e6')
+//     // await task.populate('owner').execPopulate()
+//     // console.log(task.owner)
 
-    const user = await User.findById('5c2e4dcb5eac678a23725b5b')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
+//     const user = await User.findById('5c2e4dcb5eac678a23725b5b')
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+// }
+
+const main = ()=> {
+    console.log('server runs on '+ port)
 }
 
 main()
